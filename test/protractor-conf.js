802fa67c1290fd17537123cc184e8e1bc6702633
Casey Lee <caseyl5@uw.edu>
2016-07-15 15:43:27 -0700

    singup-spec.js merge resolve

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -1,5 +1,5 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['signup-spec.js'] //run all spec files in folder
+    specs: ['*-spec.js'] //run all spec files in folder
 };
