4851d934c84dfc0a450a684cd83f99bc6eae7a25
Casey Lee <caseyl5@uw.edu>
2016-07-15 14:50:54 -0700

    password match checking functionality added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 'use strict';
 var myApp = angular.module('TestApp', []);
 
-myApp.controller('TestCtrl', ['$scope', function($scope) { 
+myApp.controller('TestCtrl', ['$scope', function ($scope) {
 
+    $scope.passwordMatch = function () {
+        $scope.testForm.confirmPassword.$setValidity("match", angular.equals($scope.pw, $scope.cpw));
+    };
+    
 }]);
