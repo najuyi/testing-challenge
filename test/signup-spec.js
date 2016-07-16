664cae2644175f74cfe85f8677cff6ac7b246084
Glenn <fyang618@uw.edu>
2016-07-15 17:51:08 -0700

    Initial name validate code

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -2,17 +2,16 @@
 
 //name test
 describe('name fields', function() {
-    var lastname = element(by.model('lastname'));
+    var lastname = element(by.model('lastName'));
     var error_message = element(by.cssContainingText('.help-block', 'last name'));
-    //valid case
-    it('should not show error message when name is valid', function() {
+    browser.get('http://localhost:8080');
 
-      
+    it('should show error message when name is invalid', function() {
+        lastname.sendKeys('name');
+        expect(error_message.isPresent()).toBe(false);
+        browser.pause();
+
+        lastname.clear();
+        expect(error_message.isPresent()).toBe(true);
     });
-
-    //invalid case
-    it('should show error message when name is invalid and touched', function () {
-        browser.get('index.html');
-    })
-
 });
