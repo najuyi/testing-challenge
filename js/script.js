c2b5811aacbb143b3e15de8bf8f7c672c750f2a9
lheston <loomisheston@hotmail.com>
2016-07-15 17:45:51 -0700

    added bday tests

--- js/script.js
+++ js/script.js
@@ -17,17 +17,20 @@ myApp.controller('TestCtrl', ['$scope', 'moment', function ($scope, moment) {
         var dateDiff = moment(now).diff(moment(input), 'y');
         if (dateDiff < 13){
             $scope.testForm.birthDate.$setValidity('old', false);
-
+          
         }
         else{
             $scope.testForm.birthDate.$setValidity('old', true);
+          
         }
         if (format == false){
             $scope.testForm.birthDate.$setValidity('valid', false);
+           
 
         }
         else{
             $scope.testForm.birthDate.$setValidity('valid', true);
+        
         }
         
     }
--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //name test
+
 describe('name fields', function() {
     var lastname = element(by.model('lastname'));
     var error_message = element(by.cssContainingText('.help-block', 'last name'));
@@ -16,3 +17,40 @@ describe('name fields', function() {
     })
 
 });
+
+describe('Validations in form birthday', function(){
+    browser.get('http://127.0.0.1:8080/');
+    it('should validate for age if under 13', function(){
+        var input = element(by.model('birthDate'));
+        var age = element(by.css('#bdayAge'));
+    
+        input.sendKeys('1/1/2015');
+        expect(age.isPresent()).toEqual(true);
+        input.clear();
+    });
+    it('should validate format of date for false input', function(){
+        
+        
+        var input = element(by.model('birthDate'));
+        var age = element(by.css('#bdayAge'));
+        var format = element(by.css('#bdayValid'));
+    
+        input.sendKeys('sdf233fk389d');
+        expect(format.isPresent()).toEqual(true);
+        input.clear();
+    });
+    it('should show no warnings when date is correct', function(){
+        
+        
+        var input = element(by.model('birthDate'));
+        var age = element(by.css('#bdayAge'));
+        var format = element(by.css('#bdayValid'));
+    
+        input.sendKeys('1/1/1985');
+        expect(format.isPresent()).toEqual(false);
+        expect(age.isPresent()).toEqual(false);
+        input.clear();
+    });
+    
+    
+})
